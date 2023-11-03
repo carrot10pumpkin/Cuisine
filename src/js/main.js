664434a9c3703aa0de7c19b5795f7f5e8fadd647
carrot10pumpkin <carrot10pumpkin@hotmail.com>
2023-11-03 10:53:49 -0400

    First Commit for Slide Test

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+var $content = $('#imgsList'); // Cache your selectors!
+
+$(".slide-control.left").hover( function loop() {
+    $content.stop().animate({  marginLeft: '-=1600' }, 5000, 'linear', function(){
+        $content.css({ marginLeft : "0px" });
+        loop();
+    });
+}, function stop() {
+    $content.stop();
+});
+
+$(".slide-control.right").hover( function loop() {
+    $content.stop().animate({  marginLeft: '+=1600' }, 5000, 'linear', function(){
+        $content.css({ marginLeft : -$content.outerWidth() });
+        loop();
+    });
+}, function stop() {
+    $content.stop();
+});
